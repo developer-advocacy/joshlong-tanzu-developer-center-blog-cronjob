afbdf6e3620240b7c4ec34861a2cd57b592cfb87
Josh Long <josh@joshlong.com>
2022-06-21 21:23:17 -0700

    up

--- src/test/java/com/example/feedprocessorv2/JoshlongMarkdownRendererTest.java
+++ src/test/java/com/example/feedprocessorv2/JoshlongMarkdownRendererTest.java
@@ -49,8 +49,8 @@ class JoshlongMarkdownRendererTest {
 		var now = this.simpleDateFormat.parse("2022-06-21");
 		var later = this.simpleDateFormat.parse("2022-06-22");
 		var description = "Hi, Spring fans! It's going to be an amazing time as I return to fabulous Berlin, Germany, for a quick look at the latest-and-greatest";
-		var md = renderer.render(new Appearance(eventTitle, now, later, "", description.strip()));
-		var html = renderer.renderMarkdownAsHtml(md);
+		var md = this.renderer.render(new Appearance(eventTitle, now, later, "", description.strip()));
+		var html = this.renderer.renderMarkdownAsHtml(md);
 		Assertions.assertTrue(html.contains("<h2>" + eventTitle + "</h2>"));
 		Assertions.assertTrue(html.contains("<p><strong>2022-06-21</strong></p>"));
 		Assertions.assertTrue(html.contains("<p>" + description + "</p>"));
