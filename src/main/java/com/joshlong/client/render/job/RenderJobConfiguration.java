20124b80ab2ad36f174e6ca9525fecd39027ecea
Josh Long <josh@joshlong.com>
2022-06-22 01:27:43 -0700

    html

--- src/main/java/com/joshlong/client/render/job/RenderJobConfiguration.java
+++ src/main/java/com/joshlong/client/render/job/RenderJobConfiguration.java
@@ -3,10 +3,14 @@ package com.joshlong.client.render.job;
 import com.joshlong.client.JoshLongClient;
 import com.joshlong.client.render.JoshLongMarkupRenderer;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
 import java.util.ArrayList;
 import java.util.LinkedHashMap;
 import java.util.List;
@@ -17,24 +21,22 @@ import java.util.stream.Collectors;
 class RenderJobConfiguration {
 
 	@Bean
-	ApplicationRunner renderJobRunner(JoshLongClient client, JoshLongMarkupRenderer renderer) {
+	ApplicationRunner renderJobRunner(JoshLongClient client, JoshLongMarkupRenderer renderer,
+			@Value("${joshlong.tdp.output-markdown-path}") File file) {
 		return args -> {
-
 			var sections = new LinkedHashMap<String, List<?>>();
 			sections.put("Podcasts", client.getPodcasts());
 			sections.put("Abstracts", client.getAbstracts());
 			sections.put("Appearances", client.getAppearances());
 			sections.put("Spring Tips", client.getSpringTips());
 			sections.put("Blog Posts", client.getBlogPosts());
-
 			var renderedOutput = new ArrayList<String>();
-
 			for (var e : sections.entrySet())
 				renderedOutput.add(renderer.renderGroup(e.getKey(), e.getValue()));
-
 			var html = (renderedOutput.stream().collect(Collectors.joining(System.lineSeparator())));
-			log.info(html);
-
+			try (var fout = new BufferedWriter(new FileWriter(file))) {
+				fout.write(html);
+			}
 		};
 	}
 
