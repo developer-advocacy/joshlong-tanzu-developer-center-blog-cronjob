9def584f7afd666348b78edf512efee2fd6825a5
Josh Long <josh@joshlong.com>
2022-06-24 17:15:22 -0700

    deploy

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -8,10 +8,10 @@ export SECRETS_FN=$HOME/${SECRETS}
 export IMAGE_NAME=gcr.io/${PROJECT_ID}/${APP_NAME}
 
 
-docker rmi -f $IMAGE_NAME
-cd $ROOT_DIR
-./mvnw -DskipTests=true spring-javaformat:apply clean package spring-boot:build-image -Dspring-boot.build-image.imageName=$IMAGE_NAME
-docker push $IMAGE_NAME
+#docker rmi -f $IMAGE_NAME
+#cd $ROOT_DIR
+#./mvnw -DskipTests=true spring-javaformat:apply clean package spring-boot:build-image -Dspring-boot.build-image.imageName=$IMAGE_NAME
+#docker push $IMAGE_NAME
 
 echo $IMAGE_NAME
 
@@ -24,4 +24,10 @@ kubectl delete secrets $SECRETS || echo "no secrets to delete."
 kubectl create secret generic $SECRETS --from-env-file $SECRETS_FN
 kubectl delete -f $ROOT_DIR/deploy/k8s/deployment.yaml || echo "couldn't delete the deployment as there was nothing deployed."
 kubectl apply -f $ROOT_DIR/deploy/k8s
+
+sleep 5
+
+echo "running the cronjob once to kick things off..."
+
+kubectl create job --from=cronjob/activity-feed-cronjob  activity-feed-cronjob-run-$RANDOM
 
