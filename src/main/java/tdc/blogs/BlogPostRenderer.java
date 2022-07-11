b00d3a894c25c8578101536a315cd00dd7e6cdf4
Josh Long <josh@joshlong.com>
2022-07-11 08:54:55 -0700

    up

--- src/main/java/tdc/blogs/BlogPostRenderer.java
+++ src/main/java/tdc/blogs/BlogPostRenderer.java
@@ -13,63 +13,64 @@ import java.text.SimpleDateFormat;
 @RequiredArgsConstructor
 class BlogPostRenderer {
 
-	private final SimpleDateFormat simpleDateFormat;
+    private final SimpleDateFormat simpleDateFormat;
 
-	private final String template = """
-			---
-			date: %s
-			description: %s
-			featured: false
-			patterns:
-			- Deployment
-			tags:
-			- Spring
-			- Kubernetes
-			- DevOps
-			- Microservices
-			- Integration
-			- Data
-			- Batch
-			- Cloud
-			team:
-			- Josh Long
-			title: '%s'
-			---
+    private final String template = """
+            ---
+            canonical: %s
+            date: %s
+            description: %s
+            featured: false
+            patterns:
+            - Deployment
+            tags:
+            - Spring
+            - Kubernetes
+            - DevOps
+            - Microservices
+            - Integration
+            - Data
+            - Batch
+            - Cloud
+            team:
+            - Josh Long
+            title: '%s'
+            ---
 
-			%s
+            %s
 
-			""";
+            """;
 
-	@SneakyThrows
-	private String readBlogPostContentFrom(URL url) {
-		var doc = Jsoup.parse(url, 5000);
-		var body = doc.getElementsByClass("blog--post");
-		var bodyHtml = body.html();
-		if (log.isDebugEnabled()) {
-			log.debug("trying to open the url " + url.toString());
-			log.debug("the body is " + bodyHtml);
-		}
-		return bodyHtml;
-	}
+    @SneakyThrows
+    private String readBlogPostContentFrom(URL url) {
+        var doc = Jsoup.parse(url, 5000);
+        var body = doc.getElementsByClass("blog--post");
+        var bodyHtml = body.html();
+        if (log.isDebugEnabled()) {
+            log.debug("trying to open the url " + url.toString());
+            log.debug("the body is " + bodyHtml);
+        }
+        return bodyHtml;
+    }
 
-	@SneakyThrows
-	public String render(BlogPost post) {
-		var description = post.description();
-		var title = this.cleanTitleForMetadata(post.title());
-		var content = readBlogPostContentFrom(post.url());
-		var templateDate = this.simpleDateFormat.format(post.published());
-		return String.format(this.template, templateDate, description, title, content);
-	}
+    @SneakyThrows
+    public String render(BlogPost post) {
+        var description = post.description();
+        var title = this.cleanTitleForMetadata(post.title());
+        var content = readBlogPostContentFrom(post.url());
+        var templateDate = this.simpleDateFormat.format(post.published());
+        return String.format(this.template, post.url().toExternalForm(), templateDate, description, title, content);
+    }
 
-	private String cleanTitleForMetadata(String title) {
-		var nb = new StringBuilder();
-		for (var c : title.toCharArray()) {
-			if (c == '\'')
-				nb.append("");
-			else
-				nb.append(c);
-		}
-		return nb.toString();
-	}
+    private String cleanTitleForMetadata(String title) {
+        var nb = new StringBuilder();
+        for (var c : title.toCharArray()) {
+            if (c == '\'')
+                nb.append("");
+            else
+                nb.append(c);
+        }
+        return nb.toString();
+    }
 
 }
